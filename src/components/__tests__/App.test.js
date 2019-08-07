dda923dad41a65d1c87f00758c3d633978017395
william <william.del.rosario@iceshop.nl>
2019-08-07 11:45:42 +0200

    Expectations for component instances

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,17 @@
 import React from  'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    const wrapped = shallow(<App />);
 
-    ReactDOM.render(<App/>, div);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-    expect(div.innerHTML).toContain('Comment Box');
+it('shows a comment list', () => {
+    const wrapped = shallow(<App />);
 
-    // cleanup
-    ReactDOM.unmountComponentAtNode(div);
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
