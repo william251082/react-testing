76a807c6b1fc06f7ee2ee40b5129da3f0b4fee68
william <william.del.rosario@iceshop.nl>
2019-08-07 11:04:49 +0200

    Using JSDOM

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from  'react';
+import ReactDOM from 'react-dom';
+import App from "../App";
+
+it('shows a comment box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App/>, div);
+
+    // cleanup
+    ReactDOM.unmountComponentAtNode(div);
+});
