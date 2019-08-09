85c1a43be3c7804392d96ea9fd7e42ccd9178158
william <william.del.rosario@iceshop.nl>
2019-08-09 17:42:15 +0200

    Fixed the broken tests

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import Root from '../Root';
+import { mount } from 'enzyme';
+import { App } from '../components/App';
+
 it('can fetch a list of comments and display them', () => {
    // Attempt to render the *entire* app
+    const wrapped = mount(
+        <Root>
+            <App/>
+        </Root>
+    );
 
     // find the 'fetchComments' button and click it
 
