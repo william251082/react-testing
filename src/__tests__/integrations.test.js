5e6f7ad9dd225bb9674cb5fd97252140d2190966
william <william.del.rosario@iceshop.nl>
2019-08-09 17:36:44 +0200

    Integration test init

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,7 @@
+it('can fetch a list of comments and display them', () => {
+   // Attempt to render the *entire* app
+
+    // find the 'fetchComments' button and click it
+
+    // Expect to find a list of comments!
+});
