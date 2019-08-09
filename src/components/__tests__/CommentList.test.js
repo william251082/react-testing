f3a58db6e88a105d7930917a201e3067bf0ae5f1
william <william.del.rosario@iceshop.nl>
2019-08-09 17:00:42 +0200

    Cheerio queries

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -17,6 +17,12 @@ beforeEach(() => {
     )
 
 });
+
 it('creates one li per comment', () => {
-    console.log(wrapped.find('li').length);
+    expect(wrapped.find('li').length).toEqual(2);
+});
+
+it('shows the text for each comment', () => {
+    expect(wrapped.render().text()).toContain('Comment 1');
+    expect(wrapped.render().text()).toContain('Comment 2');
 });
