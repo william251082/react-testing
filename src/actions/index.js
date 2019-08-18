449f5baa5f843b18cbd985eb9f32c2d95fa279d5
william <william.del.rosario@iceshop.nl>
2019-08-18 09:05:25 +0200

    Auth reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios';
 
 export function saveComment(comment) {
@@ -15,4 +15,11 @@ export function fetchComments() {
         type: FETCH_COMMENTS,
         payload: response
     }
+}
+
+export function changeAuth(isLoggedIn) {
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
+    };
 }
