a7ca5b6a3fe8590852c549560699e95676eb3f52
william <william.del.rosario@iceshop.nl>
2019-08-18 09:55:56 +0200

    Steps for building HOC

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,23 @@ import * as actions from 'actions';
 class CommentBox extends Component {
     state = { comment: '' };
 
+    // Component just got rendered
+    componentDidMount() {
+        this.shouldNavigateAway();
+    }
+
+    // Component just got updated
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+        if (!this.props.auth) {
+            console.log('I need to leave')
+        }
+    }
+
+
     handleChange = event => {
         this.setState({ comment: event.target.value });
     };
@@ -33,4 +50,8 @@ class CommentBox extends Component {
     }
 }
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+    return {auth: state.auth};
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
