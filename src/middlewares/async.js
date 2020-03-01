0fbed34e76b14eda53c8d2474c9908593dbed7e6
william <william.del.rosario@iceshop.nl>
2020-03-01 16:22:37 +0100

    forwarding actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,12 +1,10 @@
-// export default function ({ dispatch }) {
-//     return function (next) {
-//         return function (action) {
-//
-//         }
-//     }
-// }
-
-// refactor on other syntax
 export default ({ dispatch }) => next => action => {
-
-}
+    // Check to see if the action
+    // has a promise on its 'payload' property
+    // If it does, then wait for it to resolve
+    // if it doesn't, then send the action on to the
+    // next middleware
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
+};
