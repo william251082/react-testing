707fa07aac48c4abe4ed74c3d8b05bf5c3991698
william <william.del.rosario@iceshop.nl>
2020-03-01 16:18:50 +0100

    middleware syntax -- curried functions

---
+++ src/middlewares/async.js
@@ -0,0 +1,12 @@
+// export default function ({ dispatch }) {
+//     return function (next) {
+//         return function (action) {
+//
+//         }
+//     }
+// }
+
+// refactor on other syntax
+export default ({ dispatch }) => next => action => {
+
+}
