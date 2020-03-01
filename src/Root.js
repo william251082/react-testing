a164331931bec1becca5e5ebad844c6f6917d119
william <william.del.rosario@iceshop.nl>
2020-03-01 17:22:25 +0100

    emitting warnings

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import reducers from 'reducers';
 import async from "./middlewares/async";
+import stateValidator from "./middlewares/stateValidator";
 
 export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, applyMiddleware(async));
+    const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator));
     return (
         <Provider store={store}>
             {children}
--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -5,4 +5,8 @@ export default ({ dispatch, getState }) => next => action => {
     next(action);
 
     console.log(tv4.validate(getState(), stateSchema))
+
+    if (!tv4.validate(getState(), stateSchema)) {
+        console.warn('Invalid state schema detected')
+    }
 };
